78182cf3d785f2b1e481feda272092d7721c6fad
onejuneee <onejuneee721@gmail.com>
2023-06-01 17:07:18 +0900

    Feat: 자동로그인

--- src/SocialKaKao.js
+++ src/SocialKaKao.js
@@ -1,10 +1,14 @@
-import React, { useState } from "react";
+import React, {useEffect, useState} from "react";
 import KakaoLogin from "react-kakao-login";
 import axios from "axios";
 
 const SocialKaKao = () => {
     const kakaoClientId = "6dc36b91a0304922dfb9f1533c38f3fe";
     const [userInfo, setUserInfo] = useState(null);
+
+    useEffect(() => {
+        autoLogin();
+    }, []);
 
     const getKaKaoUserData = async (token) => {
         const response = await axios.get("https://kapi.kakao.com/v2/user/me", {
@@ -33,6 +37,18 @@ const SocialKaKao = () => {
 
     const kakaoOnFailure = (error) => {
         console.log(error);
+    };
+
+    const autoLogin = () => {
+        // const storedToken = localStorage.getItem('authToken');
+        const storedToken = '1234abc';
+        // if (userInfo?.kakao_account?.profile?.nickname === '김원준' && storedToken === '1234abc') {
+        if (storedToken === '1234abc') {
+            console.log('Automatically logged in');
+            alert('자동로그인 되었습니다');
+        } else {
+            console.log('Token is not valid');
+        }
     };
 
     return (
